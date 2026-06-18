a62f7e816535dd0ba9e5a5b6fdd10767f94a5ebe
Emre Kağan Erde <emrekaganerde@outlook.com>
2026-06-18 11:13:11 +0300

    feat: integrate NBomber and implement BookStore load tests

---
+++ src/DotNetPerformanceLab.LoadTests/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using DotNetPerformanceLab.LoadTests.BookStoreApi;
+
+var host = Host.CreateDefaultBuilder()
+    .ConfigureServices(services =>
+    {
+        services.AddHttpClient("BookStoreApi", client =>
+        {
+           client.BaseAddress = new Uri("http://localhost:5184");
+        });
+    })
+    .Build();
+
+var factory = host.Services.GetRequiredService<IHttpClientFactory>();
+await BookStoreLoadTest.RunAsync(factory);
